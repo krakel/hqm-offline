86aaeb66939490c6739de82eb30612a60d47a8a4
krakel <uwe-git@doerl.de>
2015-07-18 11:36:49 +0200

    correct ant file

--- convert.bat
+++ convert.bat
@@ -1,8 +1,7 @@
 @echo off
 
 rem if you want a special java version
-set JAVA_HOME=%ProgramFiles%\java\jre7
-
-set path=.;%JAVA_HOME%\bin
+rem set JAVA_HOME=%ProgramFiles%\java\jre7
+rem set path=.;%JAVA_HOME%\bin
 	
 java -jar hqm_convert.jar
